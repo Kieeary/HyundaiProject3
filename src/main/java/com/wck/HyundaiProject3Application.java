fc0f8173a84d2a0dda1599f9ac078055036af082
HanulKim <hanscnnee@gmail.com>
2023-01-27 16:04:08 +0900

    tmp

---
+++ src/main/java/com/wck/HyundaiProject3Application.java
@@ -0,0 +1,13 @@
+package com.wck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyundaiProject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyundaiProject3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/wck/HyundaiProject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyundaiProject3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
